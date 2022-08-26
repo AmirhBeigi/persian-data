5cc7338a842fbb1951370ad8adb2ffcf1f3e4333
Amirhossein <dev.amirhbeigi@gmail.com>
2022-08-26 19:23:03 +0430

    feat: add prefixCountriesPhoneNumberAndIran

--- data/phone/index.ts
+++ data/phone/index.ts
@@ -1,4 +1,5 @@
 import prefixIranianPhone from './prefixIranianPhone.json';
+import prefixCountriesPhoneNumberAndIran from './prefixCountriesPhoneNumberAndIran.json';
 
 export const phone = [
   {
@@ -6,5 +7,11 @@ export const phone = [
     data: prefixIranianPhone,
     githubSrc:
       'https://github.com/AmirhBeigi/persian-data/blob/fdcea6f01a6848041c59b0e8d794b44275fcc183/data/phone/prefixIranianPhone.json',
+  },
+  {
+    title: 'Prefix Countries Phone Number And Iran',
+    data: prefixCountriesPhoneNumberAndIran,
+    githubSrc:
+      'https://github.com/AmirhBeigi/persian-data/blob/fdcea6f01a6848041c59b0e8d794b44275fcc183/data/phone/prefixCountriesPhoneNumberAndIran.json',
   },
 ];
