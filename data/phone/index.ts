228989031adbf9cd2b9d88a072f514e62f03dbdc
Amirhossein <dev.amirhbeigi@gmail.com>
2022-08-26 19:16:16 +0430

    chore: typo

--- data/phone/index.ts
+++ data/phone/index.ts
@@ -1,10 +1,10 @@
-import IranianPrefixPhone from './IranianPrefixPhone.json';
+import prefixIranianPhone from './prefixIranianPhone.json';
 
 export const phone = [
   {
-    title: 'Iranian Prefix Phone',
-    data: IranianPrefixPhone,
+    title: 'Prefix Iranian Phone',
+    data: prefixIranianPhone,
     githubSrc:
-      'https://github.com/AmirhBeigi/persian-data/blob/fdcea6f01a6848041c59b0e8d794b44275fcc183/data/phone/IranianPrefixPhone.json',
+      'https://github.com/AmirhBeigi/persian-data/blob/fdcea6f01a6848041c59b0e8d794b44275fcc183/data/phone/prefixIranianPhone.json',
   },
 ];
