fc448770a9b2d0a20e5830156d6f471dda8cce5d
Amirhossein <dev.amirhbeigi@gmail.com>
2022-08-26 19:32:53 +0430

    fix: change links

--- data/phone/index.ts
+++ data/phone/index.ts
@@ -3,15 +3,13 @@ import prefixCountriesPhoneNumberAndIran from './prefixCountriesPhoneNumberAndIr
 
 export const phone = [
   {
-    title: 'Prefix Iranian Phone',
+    title: 'Prefix Provinces Iran Phone',
     data: prefixIranianPhone,
-    githubSrc:
-      'https://github.com/AmirhBeigi/persian-data/blob/fdcea6f01a6848041c59b0e8d794b44275fcc183/data/phone/prefixIranianPhone.json',
+    githubSrc: 'https://github.com/AmirhBeigi/persian-data/blob/main/data/phone/prefixIranianPhone.json',
   },
   {
     title: 'Prefix Countries Phone Number And Iran',
     data: prefixCountriesPhoneNumberAndIran,
-    githubSrc:
-      'https://github.com/AmirhBeigi/persian-data/blob/fdcea6f01a6848041c59b0e8d794b44275fcc183/data/phone/prefixCountriesPhoneNumberAndIran.json',
+    githubSrc: 'https://github.com/AmirhBeigi/persian-data/blob/main/data/phone/prefixCountriesPhoneNumberAndIran.json',
   },
 ];
